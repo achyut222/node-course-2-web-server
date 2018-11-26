f837cdc94daecec60d60d06a6b4525b051cc6e05
Achyut Behera <ab@Achyuts-MacBook-Air.local>
2018-11-26 21:01:23 +0530

    setup start script and heroku port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const hbs = require('hbs');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 hbs.registerPartials(__dirname+'/views'+'/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname+'/public'));
@@ -37,4 +39,6 @@ app.get('/bad', (req, res)=>{
     errorMsg:'bad request'
   });
 });
-app.listen(3000);
+app.listen(port,()=>{
+  console.log(`server is up on running ${port}`);
+});
